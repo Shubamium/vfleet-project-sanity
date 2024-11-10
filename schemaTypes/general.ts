582a0cc2e904cad59343e38ff2085c9fcfcfbf5c
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-11-10 12:06:14 +0700

    Add cycle

--- schemaTypes/general.ts
+++ schemaTypes/general.ts
@@ -22,5 +22,29 @@ export default defineType({
       title: 'A2 Image',
       type: 'image',
     }),
+    defineField({
+      name: 'hero_section',
+      title: 'Hero Section Art List',
+      description: 'Arts for the hero section on the home page',
+      type: 'array',
+      of: [
+        defineField({
+          name: 'art',
+          type: 'image',
+        }),
+      ],
+    }),
+    defineField({
+      name: 'talent_section',
+      title: 'Hero Section Art List',
+      description: 'Arts for the talent section on the home page',
+      type: 'array',
+      of: [
+        defineField({
+          name: 'art',
+          type: 'image',
+        }),
+      ],
+    }),
   ],
 })
